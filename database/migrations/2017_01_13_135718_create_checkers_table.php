2b277d353cc077c8291f2f2d930621b08b0f58db
Daniel <daniel.vernberg@gmail.com>
2017-01-13 15:01:05 +0100

    model and migration for checker

--- database/migrations/2017_01_13_135718_create_checkers_table.php
+++ database/migrations/2017_01_13_135718_create_checkers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCheckersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,10 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at')->nullable();
+        Schema::create('checkers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('checkers-nr');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +27,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('checkers');
     }
 }

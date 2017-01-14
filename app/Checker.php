aba036340b39d37e37fb65d3d8446bec149648aa
Daniel <daniel.vernberg@gmail.com>
2017-01-14 13:45:26 +0100

    create a scope for getting all checkers entries for the last 30 days

--- app/Checker.php
+++ app/Checker.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Checker extends Model
 {
-    //
+  public function scopeGetSortedLast30Days($query)
+  {
+    return $query->where('created_at', '>', Carbon::now()->subDays(30))
+        ->orderBy('created_at')
+        ->get();
+  }
 }
